a91225eb4e04e5ae689855bba9da80b6e414b130
Manel Morales Rivera <manelmoralesrivera@gmail.com>
2026-05-03 21:26:31 +0200

    Instalacion de checkstyle y actualización de ci.yml

--- src/main/java/com/dam/calculatorMRJM/CalculatorMrjmApplication.java
+++ src/main/java/com/dam/calculatorMRJM/CalculatorMrjmApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorMrjmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorMrjmApplication.class, args);
-	}
+public static void main(String[] args) {
+SpringApplication.run(CalculatorMrjmApplication.class, args);
+}
 
 }
