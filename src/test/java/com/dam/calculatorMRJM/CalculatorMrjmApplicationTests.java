cefad91c805887d2ab9135a1a4277f8f28461a58
Manel Morales Rivera <manelmoralesrivera@gmail.com>
2026-05-03 20:57:11 +0200

    Calculator + Controller + build.gradle + Tests

--- src/test/java/com/dam/calculatorMRJM/CalculatorMrjmApplicationTests.java
+++ src/test/java/com/dam/calculatorMRJM/CalculatorMrjmApplicationTests.java
@@ -1,13 +1,19 @@
 package com.dam.calculatorMRJM;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CalculatorMrjmApplicationTests {
+class CalculatorMRJMApplicationTests {
+    private Calculator calculator = new Calculator();
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(3, 2));
+    }
 }
