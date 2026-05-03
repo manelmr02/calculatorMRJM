dec28d4c04aa5eed53dae025e1cc5b659a1583f1
Manel Morales Rivera <manelmoralesrivera@gmail.com>
2026-05-03 20:12:16 +0200

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/dam/calculatorMRJM/CalculatorMrjmApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorMRJM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorMrjmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorMrjmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dam/calculatorMRJM/CalculatorMrjmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorMRJM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorMrjmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
